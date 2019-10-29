4a1873822eddfef8dc1c3d7b2584da5e83e21ffe
pilar <pilar.rus.izquierdo@gmail.com>
2019-10-29 19:18:28 +0100

    home_adventures

---
+++ src/containers/adventures.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Adventures = () => {
+  return <div className="red">Hello from Adventures<h2>Nuestras aventuras</h2></div>;
+};
+
+export default Adventures;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Adventures from "../containers/adventures";
 
 const Home = () => {
-  return <div className="azul">Hello from homepage</div>;
+  return (
+    <div className="azul">
+      Hello from homepage
+      <Adventures />
+    </div>
+  );
 };
 
 export default Home;
