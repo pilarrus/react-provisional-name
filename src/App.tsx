4d6f873841b256eb476f060a5da24013c325ec41
Elisa <elisagcubero@gmail.com>
2019-10-29 13:22:17 +0100

    añadadido Social

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Adventures from "./pages/Adventures";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
-import Adventures from "./pages/Adventures";
+import Social from "./pages/Social";
 
 const App: React.FC = () => {
   return (
     <div data-testid="main">
       <Home />
       <Adventures />
+      <Social />
       <Error />
     </div>
   );
