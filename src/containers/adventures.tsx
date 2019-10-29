7a32f428d71260427075aa8460778074fe570005
pilar <pilar.rus.izquierdo@gmail.com>
2019-10-29 20:49:19 +0100

    probando adventures

--- src/containers/adventures.tsx
+++ src/containers/adventures.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import image from "../images/linux.jpg";
 
 const Adventures = () => {
   return (
     <div className="red">
       Hello from Adventures
-      <h2>Nuestras aventuras</h2>
-      <img src={image} alt="linux" />
+      <div>
+        <h2>Nuestras aventuras</h2>
+      </div>
+      <div>
+        <div>Aventura</div>
+        <div>Aventura</div>
+        <div>Aventura</div>
+      </div>
+      <button>Ver más aventuras</button>
     </div>
   );
 };
