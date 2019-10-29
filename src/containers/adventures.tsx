008fe6ac56f29d268238ca912efb40e739caf482
pilar <pilar.rus.izquierdo@gmail.com>
2019-10-29 20:37:27 +0100

    probar imagen

--- src/containers/adventures.tsx
+++ src/containers/adventures.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import image from "../images/linux.jpg";
 
 const Adventures = () => {
-  return <div className="red">Hello from Adventures<h2>Nuestras aventuras</h2></div>;
+  return (
+    <div className="red">
+      Hello from Adventures
+      <h2>Nuestras aventuras</h2>
+      <img src={image} alt="linux" />
+    </div>
+  );
 };
 
 export default Adventures;
