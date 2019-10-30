82a67565d6e4a1d182f6bc0609bb32bc4784bc8b
Elisa <elisagcubero@gmail.com>
2019-10-30 12:34:03 +0100

    [social_home]: transformado a class component

--- src/containers/SocialHome/SocialHome.tsx
+++ src/containers/SocialHome/SocialHome.tsx
@@ -1,46 +1,68 @@
 import React from "react";
 import image from "../../images/coffee.jpg";
 
-const Social = () => {
-  return (
-    <div className="social">
-      <div className="social__info-box">
-        <h1 className="social__info-box-title">
-          Únete a nuestra red Social de aventurer@s
-        </h1>
-        <div className="social__info-box-users">
-          <p>Ya soy usuario:</p>
-          <button>+</button>
-          <p>Registrarme:</p>
-          <button>+</button>
-        </div>
-      </div>
 
-      <div className="social__img-box">
-        <img src={image} />
-      </div>
-      <div className="social__login-box">
-        <label> Usuario y contraseña</label>
-        <form>
-          <label>Usuario:</label>
-          <input type="text"></input>
-          <br />
-          <label>Contraseña:</label>
-          <input type="password"></input>
-        </form>
-      </div>
-      <div className="social__form-box">
-        <label> Registro</label>
-        <form>
-          <label>Nombre:</label>
-          <input type="text"></input>
-          <br />
-          <label>Apellidos:</label>
-          <input type="text"></input>
-        </form>
-      </div>
+class Social extends React.Component {
+
+  state = {
+    clickLogin: false,
+    clickRegister: false
+  }
+
+  render() {
+
+    return (
+
+
+      <div className="social" >
+
+        <div className="social__info-box">
+          <h1 className="social__info-box-title">
+            Únete a nuestra red de aventurer@s
+        </h1>
+          <div className="social__info-box-users">
+            <p>Ya soy usuario:</p>
+            <button>+</button>
+            <p>Registrarme:</p>
+            <button>+</button>
+          </div>
+        </div>
+
+        <div className="social__img-box">
+          <img src={image} alt="coffee" />
+        </div>
+        <div className="social__login-box">
+          <label> Usuario y contraseña</label>
+          <form>
+            <label>Usuario:</label>
+            <input type="text"></input>
+            <br />
+            <label>Contraseña:</label>
+            <input type="password"></input>
+          </form>
+        </div>
+        <div className="social__form-box">
+          <label> Registro</label>
+          <form>
+            <label>Nombre:</label>
+            <input type="text"></input>
+            <br />
+            <label>Apellidos:</label>
+            <input type="text"></input>
+          </form>
+        </div>
+      </div >
+    )
+
+  }
+
+  /*import React from "react";
+  
+  export const Social = () => {
+    <div>
+      Hola soy Social;
     </div>
-  );
-};
+  }*/
+}
 
 export default Social;
