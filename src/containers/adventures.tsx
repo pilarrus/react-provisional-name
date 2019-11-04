48b2f2ab80d36a94877e1678f700d9d36f21f28a
Pilar <pilar.rus.izquierdo@gmail.com>
2019-11-04 13:20:45 +0100

    adventures

--- src/containers/adventures.tsx
+++ src/containers/adventures.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Adventure from "./adventure";
 import climbing from "../images/adventure_climbing.jpg";
-import zip_lines from "../images/adventure_zip_lines.jpg";
 import rafting from "../images/adventure_rafting.jpg";
+import zip_lines from "../images/adventure_zip_lines.jpg";
+import Adventure from "./adventure";
 
 const Adventures = () => {
-  let adventuresName = [
+  let adventures = [
     { name: "Escalada", photo: climbing },
     { name: "Tirolinas", photo: zip_lines },
     { name: "Rafting", photo: rafting }
@@ -16,8 +16,12 @@ const Adventures = () => {
         <h2 className="center">Nuestras aventuras</h2>
       </div>
       <div className="row">
-        {adventuresName.map(obj => (
-          <Adventure key={obj.name} name={obj.name} photo={obj.photo} />
+        {adventures.map(adventure => (
+          <Adventure
+            key={adventure.name}
+            name={adventure.name}
+            photo={adventure.photo}
+          />
         ))}
       </div>
       <button>Ver más aventuras</button>
