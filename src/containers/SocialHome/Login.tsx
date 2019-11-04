5b598cc0747e29004d75567ee1ed03b2197694e3
elisagc <elisagcubero@gmail.com>
2019-11-04 18:49:08 +0100

    incluida animacion con paquete ANIMATION

--- src/containers/SocialHome/Login.tsx
+++ src/containers/SocialHome/Login.tsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { Animated } from "react-animated-css";
 
 export const Login = () => {
   return (
-    <div className="social__right-login">
-      <form className="form">
-        <input type="text" placeholder="Email" className="form-input"></input>
-        <input type="password" placeholder="Contraseña" className="form-input"></input>
-        <div className="form-submit">
-          <input type="submit" value="Acceder" />
-        </div>
-      </form>
-    </div >
-  )
-}
+    <Animated
+      animationIn="fadeIn"
+      animationOut="flash"
+      animationInDuration={2000}
+      isVisible={true}
+    >
+      <div className="social__right-login">
+        <form className="form">
+          <input type="text" placeholder="Email" className="form-input"></input>
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className="form-input"
+          ></input>
+          <div className="form-submit">
+            <input type="submit" value="Acceder" />
+          </div>
+        </form>
+      </div>
+    </Animated>
+  );
+};
 
 export default Login;
--- src/containers/SocialHome/Register.tsx
+++ src/containers/SocialHome/Register.tsx
@@ -2,15 +2,8 @@ import React from "react";
 import Form from "./Form";
 import Login from "./Login";
 
-const Register: React.FC<{ login: boolean }> = ({ login }) =>
-
-  (
-    <div className="social__right">
-      {login ?
-        <Login />
-        :
-        <Form />}
-    </div>
-  )
+const Register: React.FC<{ login: boolean }> = ({ login }) => (
+  <div className="social__right">{login ? <Login /> : <Form />}</div>
+);
 
 export default Register;
