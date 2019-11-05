efbc21144e8de77bcc993ca4d638894fd0b6aa38
elisagc <elisagcubero@gmail.com>
2019-11-05 20:06:07 +0100

    creado weather

---
+++ src/containers/Weather/WeatherHome.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class WeatherHome extends React.Component {
+  render() {
+    return <div className="weather">soy weather</div>;
+  }
+}
+
+export default WeatherHome;
