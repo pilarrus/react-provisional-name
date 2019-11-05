a97de7006830e2431175de0c4fe8a595c0fcd733
Elisa <elisagcubero@gmail.com>
2019-11-05 09:44:26 +0100

    añadida funcion vacia a button para que sea opcional

--- src/containers/SocialHome/Button.tsx
+++ src/containers/SocialHome/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button: React.FC<{ title: string, nameClass: string, changeState?: () => void }> = ({ title, nameClass, changeState }) => (
+const Button: React.FC<{ title: string, nameClass: string, changeState?: () => void }> = ({ title, nameClass, changeState = () => { } }) => (
   <div className={nameClass}>
     <button onClick={changeState} >{title}</button>
   </div>
