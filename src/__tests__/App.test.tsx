3b1e3a8c0907802213e87c2958bd09ce2c7d8b17
elisagc <elisagcubero@gmail.com>
2019-11-05 18:26:15 +0100

    ?

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,5 +4,5 @@ import App from "../App";
 
 test("Hello test", () => {
   const { getByTestId } = render(<App />);
-  expect(getByTestId("main").children.length).toBe(1);
+  expect(getByTestId("main").children.length).toBe(2);
 });
