1702f132cbe74eb1eafc461e501ed6126fca541b
Elisa <elisagcubero@gmail.com>
2019-11-06 12:03:47 +0100

    corregida errata config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,4 @@ const config = {
   }
 }
 
-
 export default config;
--- src/containers/Weather/WeatherHome.tsx
+++ src/containers/Weather/WeatherHome.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import config from "../../confing";
+import config from "../../config";
 
 const postalCode = "28022";
 
 class WeatherHome extends React.Component {
   state = {
-    loading: true
+    loading: true,
+    data: []
   };
-
+  //https://github.com/paroar/React_group/blob/containers/src/containers/Catalogue.tsx
   async componentDidMount() {
     console.log(">>>>>>", process.env)
-    const url =
-      `${config.aemet.url}/${postalCode}?api_key=${config.aemet.apiKey}`;
+
+    // fichero config.ts
+    const url = `${config.aemet.url}/${postalCode}?api_key=${config.aemet.apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
+
     const url2 = data.datos;
     const response2 = await fetch(url2);
-    const data2 = await response2.json()
-    console.log(data2)
+    const data2 = await response2.json();
+
+    console.log(data2);
   }
 
   render() {
-    return <div className="weather">soy weather</div>;
+    return <div className="weather"></div>;
   }
 }
 
