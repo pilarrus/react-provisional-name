9205b8b458dc0eef5c05f0df16138e9c7d833e72
pilar <pilar.rus.izquierdo@gmail.com>
2019-11-07 20:58:27 +0100

    sass

--- src/components/Adventure.tsx
+++ src/components/Adventure.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-//import { typeAdventure } from "../types/adventure";
 
 const AdventureComponent = (props: { name: string, photo: string, info: string; }) => {
   return (
-    <div className="col-1-of-3 adventure" data-testid="adventure">
+    <div className="adventure" data-testid="adventure">
       <div>
         <img src={props.photo} alt="adventure"/>
       </div>
-      <div>
-        <h3>{props.name}</h3>
-      </div>
+      <h3>{props.name}</h3>
       <div>
         {props.info}
       </div>
