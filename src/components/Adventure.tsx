ee177b0e6913592ce127075e8c4a5cd988fbfe81
Pilar <pilar.rus.izquierdo@gmail.com>
2019-11-08 11:53:19 +0100

    conflicto

--- src/components/Adventure.tsx
+++ src/components/Adventure.tsx
@@ -2,13 +2,8 @@ import React from "react";
 
 const AdventureComponent = (props: { name: string, photo: string, info: string; }) => {
   return (
-<<<<<<< HEAD
-    <div className="col-1-of-3 adventure" data-testid="adventure">
-      <div >
-=======
     <div className="adventure" data-testid="adventure">
       <div>
->>>>>>> 9205b8b458dc0eef5c05f0df16138e9c7d833e72
         <img src={props.photo} alt="adventure"/>
       </div>
       <h3>{props.name}</h3>
