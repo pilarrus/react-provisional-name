0c0f9ceaa078fc5e2589ad58b7253812f758b0a6
Pilar <pilar.rus.izquierdo@gmail.com>
2019-11-14 14:16:21 +0100

    merge con nuevoestilo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,9 @@ import WeatherPrueba from "../containers/Weather/WPrueba";
 import { convertDegreesToThermalSensation } from "../utils/functions";
 
 type dataTemperatureType = {
-  "name": string;
-  "max": number;
-  "min": number;
+  name: string;
+  max: number;
+  min: number;
 };
 
 const madrid = {
@@ -50,20 +50,18 @@ class Home extends React.Component {
         var temperaturesMunicipality = {
           name: data[0]["nombre"],
           max: data[0]["prediccion"]["dia"][0]["temperatura"]["maxima"],
-          min: data[0]["prediccion"]["dia"][0]["temperatura"]["minima"],
+          min: data[0]["prediccion"]["dia"][0]["temperatura"]["minima"]
         };
         this.setState({ dataTemperature: temperaturesMunicipality, lenght: 1 });
       });
-
   };
 
   render() {
     if (this.state.lenght > 0) {
-
-
       let degree = this.state.dataTemperature.max;
-      let thermalSensationAPI: string = convertDegreesToThermalSensation(degree);
-
+      let thermalSensationAPI: string = convertDegreesToThermalSensation(
+        degree
+      );
 
       return (
         <div data-testid="home-page">
@@ -73,7 +71,6 @@ class Home extends React.Component {
         </div>
       );
     } else {
-
       return (
         <div className="weather-charge">
           <p>Cargando...</p>
@@ -81,6 +78,6 @@ class Home extends React.Component {
       );
     }
   }
-};
+}
 
 export default Home;
