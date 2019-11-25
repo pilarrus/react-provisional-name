8befc03dc581ec37d01577ef85e198e43f546cee
spindado <sergio.pindadog@gmail.com>
2019-11-25 19:33:30 +0100

    nuevo slider;creacion de nuevo Branch a partir del branch nuevoestilo

---
+++ src/components/slider/images.tsx
@@ -0,0 +1,7 @@
+export const images = [
+  "https://unsplash.com/photos/6F98shIQysI",
+  "https://unsplash.com/photos/WdBQHcIiSIw",
+  "https://unsplash.com/photos/whq3kLnLdRA",
+  "https://unsplash.com/photos/48e-EBtBtmw",
+  "https://unsplash.com/photos/G0FsO2Ca8nQ"
+];
