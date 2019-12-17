c54438bdb542e73b27fae6e2006f9899cc4d5eb2
pilar <pilar.rus.izquierdo@gmail.com>
2019-12-17 18:31:46 +0100

    [ButtonRainbow] quitado comentario

--- src/components/Reusable/ButtonRainbow.tsx
+++ src/components/Reusable/ButtonRainbow.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 
-/*const ButtonRainbow = (text: string) => {
-  return (
-    <div className="btn--rainbow">
-      <button type="submit" className="btn">{text}</button>
-    </div>
-  );
-};*/
-
 const ButtonRainbow: React.FC<{ text: string }> = ({ text }) => (
   <div className="btn--rainbow">
     <button type="submit" className="btn">
