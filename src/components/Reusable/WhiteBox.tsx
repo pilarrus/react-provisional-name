7667c00e6c0a293a5b9c63b4ca685b1932fc0e62
elisagc <elisagcubero@gmail.com>
2019-12-18 09:10:44 +0100

    comenzado estilo perfil

---
+++ src/components/Reusable/WhiteBox.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Box: React.FC = (children: React.ReactNode) => {
+  return <div>{children}</div>;
+};
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
 import dataUsers from "../fake-data/usersRegisters";
-import noimage from "../images/profile/noimage2.png";
-
+import person1 from "../images/profile/person1.jpg";
 export const Profile: React.FC<RouteComponentProps> = RouteComponentProps => {
   let data = RouteComponentProps.location.state; // datos que recibo del formulario de registro
 
@@ -20,7 +19,10 @@ export const Profile: React.FC<RouteComponentProps> = RouteComponentProps => {
   console.log(data);
   return (
     <div className="profile">
-      <div className="profile__box">
+      {
+        //caja nombre y foto
+      }
+      <div className="profile__box name_box">
         <div className="profile__name">
           <h1>
             {data.gender === "male"
@@ -34,13 +36,65 @@ export const Profile: React.FC<RouteComponentProps> = RouteComponentProps => {
         </div>
 
         <div className="profile__picture">
-          <img src={noimage} alt="you" />
+          <img src={person1} alt="you" />
         </div>
       </div>
 
+      {
+        //fin caja nombre y foto
+      }
+
       <div className="profile__friends">
-        <div className="profile__friends-groups">Mis grupos</div>
-        <div className="profile__friends-friends">Mis Amigos</div>
+        <div className="profile__friends-groups groups_box">
+          <h1>Este mes:</h1>
+          <table>
+            <tr>
+              <th>Fecha</th>
+              <th>Día de la semana</th>
+              <th>Aventura</th>
+              <th>Lugar</th>
+              <th>Hora</th>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>Viernes</td>
+              <td>Escalada</td>
+              <td>Guadarrama</td>
+              <td>10:00</td>
+            </tr>
+            <tr>
+              <td>Día 13</td>
+              <td>Viernes</td>
+              <td>Escalada</td>
+              <td>Guadarrama</td>
+              <td>10:00</td>
+            </tr>
+            <tr>
+              <td>Día 13</td>
+              <td>Viernes</td>
+              <td>Escalada</td>
+              <td>Guadarrama</td>
+              <td>10:00</td>
+            </tr>
+          </table>
+        </div>
+        <div className="profile__friends-friends friends_box">
+          <h1>Amig@s</h1>
+          <div>
+            <div className="friends_img">
+              <span>Pablo666</span>
+              <img src={person1} />
+            </div>
+            <div className="friends_img">
+              <span>Pablo666</span>
+              <img src={person1} />
+            </div>
+            <div className="friends_img">
+              <span>Pablo666</span>
+              <img src={person1} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
