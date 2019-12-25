44ec8dde7fc50296da5f5ca2464703543fd8d0fb
elisagc <elisagcubero@gmail.com>
2019-12-25 20:33:02 +0100

    modificado estilo

--- src/components/Profile/Groups.tsx
+++ src/components/Profile/Groups.tsx
@@ -5,17 +5,31 @@ export const ProfileGroups: React.FC = () => {
   const [openGroups, setGroups] = useState(false);
   const [createAdventure, setCreateAdventure] = useState(false);
 
+  const style = {
+    fontSize: "2rem"
+  };
   console.log(openGroups);
   return (
     <div className="profile__groups">
-      <h1 className="profile-title">Localiza o crea tus Aventuras</h1>
-      <button onClick={() => setGroups(!openGroups)}>
-        Ver grupos disponibles
+      <button
+        onClick={() => setGroups(!openGroups)}
+        className="profile__groups-b1 heading heading--stroke heading--shadow"
+        style={style}
+      >
+        Aventuras de la semana
       </button>
-      <button onClick={() => setCreateAdventure(!createAdventure)}>
+      <button
+        onClick={() => setCreateAdventure(!createAdventure)}
+        className="profile__groups-b1 heading heading--stroke heading--shadow"
+        style={style}
+      >
         Crear aventura
       </button>
-      {openGroups ? <Modal /> : null}
+      {openGroups ? (
+        <Modal info="view" changestate={setGroups} />
+      ) : createAdventure ? (
+        <Modal info="create" changestate={setCreateAdventure} />
+      ) : null}
     </div>
   );
 };
--- src/components/Profile/Modal.tsx
+++ src/components/Profile/Modal.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Groups from "../../pages/Groups";
+import AddGroup from "../AddGroup";
 
-export const Modal: React.FC = () => {
-  return (
-    <div className="profile__modal">
+export const Modal: React.FC<{
+  info: string;
+  changestate: React.Dispatch<React.SetStateAction<boolean>>;
+}> = props => {
+  const changeState = () => {
+    props.changestate(true);
+  };
+  console.log(props.info);
+  console.log(props.changestate);
+  const insideModal = (info: string) => {
+    return info === "view" ? (
       <Groups />
-    </div>
-  );
+    ) : info === "create" ? (
+      <AddGroup changeState={changeState} />
+    ) : null;
+  };
+  return <div className="profile__modal">{insideModal(props.info)}</div>;
 };
 
 export default Modal;
