f6fdd7cfd8084b654c2a10696163eb75fc00e659
Pilar <pilar.rus.izquierdo@gmail.com>
2020-01-14 14:14:23 +0100

    Sort groups

--- src/components/Group/Groups.tsx
+++ src/components/Group/Groups.tsx
@@ -4,14 +4,30 @@ import Title from "../Reusable/Title";
 import ButtonRainbow from "../Reusable/ButtonRainbow";
 import AddGroup from "./AddGroup";
 import Group from "./Group";
+//import groups from "../../fake-data/groups";
 
 type PropsCompGroups = {
   groups: Groups2;
   showAll: boolean;
 };
 
+const sorted = (x: string, y: string) => {
+  if(x < y) return -1;
+  else if (x > y) return 1;
+  return 0;
+}
+
 const Groups: React.FC<PropsCompGroups> = ({ groups, showAll }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [sort, setSort] = useState("alphabetical");
+  console.log("sort>>",sort);
+  console.log("groups>>>",groups);
+
+  const sortByTypeAdventure = (groups: Groups2) => {
+    let keys = Object.keys(groups);
+    return keys.sort(sorted);
+  }
+  console.log(">>>>", sortByTypeAdventure(groups));
 
   let adventureName = "";
 
@@ -55,6 +71,12 @@ const Groups: React.FC<PropsCompGroups> = ({ groups, showAll }) => {
   return (
     <section className="groups">
       <Title title={`Grupos${adventureName}`} />
+      <select name="" id="" defaultValue="Default" onChange={e => setSort(e.target.value)}>
+        <option value="Default" disabled>Ordena por</option>
+        <option value="alphabetical">Orden alfabético</option>
+        <option value="latestDate">Fecha más reciente</option>
+        <option value="typeAdventure">Tipo de aventura</option>
+      </select>
       <div className="groups__container">
         <div className="groups__container--box">{showGroups}</div>
         <ButtonRainbow
