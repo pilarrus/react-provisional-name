207b8106d021aca4e47bba9dbfc2fb3cb6196237
Pilar <pilar.rus.izquierdo@gmail.com>
2020-02-12 11:59:06 +0100

    Added pili branch

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -9,7 +9,6 @@ import Error from "../pages/Error";
 import Groups from "../pages/Groups";
 import Home from "../pages/Home";
 import { Profile } from "../pages/Profile";
-import Footer from "./Footer";
 
 const Main: React.FC<{ sidebar: boolean }> = ({ sidebar }) => {
   let style = "";
@@ -29,7 +28,6 @@ const Main: React.FC<{ sidebar: boolean }> = ({ sidebar }) => {
         <Route exact path="/login" component={LoginRegister} />
         <Route component={Error} />
       </Switch>
-     
     </div>
   );
 };
