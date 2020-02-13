70ac188dd6829934dece5d453bb36484b3d3e16f
Pilar <pilar.rus.izquierdo@gmail.com>
2020-02-13 12:36:48 +0100

    added enviroments

---
+++ src/enviroments/enviroment.tsx
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/database";
+
+console.log(">>>>>>", process.env);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+export default fire;
