7788404993f9a37d1f17516c6f6bf67949536160
Elisa <elisagcubero@gmail.com>
2020-02-17 14:09:59 +0100

    responsive title

--- src/components/Reusable/Title.tsx
+++ src/components/Reusable/Title.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 
 const name = (title: string) => {
-  return title.toLowerCase().includes('aventuras')
-    ? "redclass" : title.toLowerCase().includes('grupos')
-      ? "clearblueclass" : title.toLowerCase().includes('contacto')
-        ? "greenclass" : title.toLowerCase().includes('nosotros')
-          ? "purpleclass" : title.toLowerCase().includes('home')
-            ? "yellowclass" : "defaultclass";
+  return title.toLowerCase().includes("aventuras")
+    ? "redclass"
+    : title.toLowerCase().includes("grupos")
+    ? "clearblueclass"
+    : title.toLowerCase().includes("contacto")
+    ? "greenclass"
+    : title.toLowerCase().includes("nosotros")
+    ? "purpleclass"
+    : title.toLowerCase().includes("home")
+    ? "yellowclass"
+    : "defaultclass";
+};
 
-}
-
-const Title: React.FC<{ title: string }> = ({
-  title,
-}) => (
-
-    <div>
-      <h1 className={name(title)}>{title}</h1>
-    </div>
-  );
+const Title: React.FC<{ title: string }> = ({ title }) => (
+  <div className="divtitle">
+    <h1 className={name(title)}>{title}</h1>
+  </div>
+);
 
 export default Title;
