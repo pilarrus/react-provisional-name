5e7177bba43bf584876d72ac57bee7e72116e350
elisagc <elisagcubero@gmail.com>
2020-02-18 19:54:10 +0100

    titulos con color sidebar

--- src/components/Reusable/Title.tsx
+++ src/components/Reusable/Title.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 
 const name = (title: string) => {
-  return title.toLowerCase().includes('aventuras')
-    ? "redclass" : title.toLowerCase().includes('grupos')
-      ? "clearblueclass" : title.toLowerCase().includes('contacto')
-        ? "greenclass" : title.toLowerCase().includes('nosotros')
-          ? "purpleclass" : title.toLowerCase().includes('home')
-            ? "yellowclass" : "defaultclass";
+  return title.toLowerCase().includes("aventuras")
+    ? "clearblueclass"
+    : title.toLowerCase().includes("grupos")
+    ? "blueclass"
+    : title.toLowerCase().includes("contacto")
+    ? "purpleclass"
+    : title.toLowerCase().includes("nosotros")
+    ? "greenclass"
+    : title.toLowerCase().includes("accede a nuestra red social")
+    ? "secondpurpleclass"
+    : "defaultclass";
+};
 
-}
-
-const Title: React.FC<{ title: string }> = ({
-  title,
-}) => (
-
-    <div>
-      <h1 className={name(title)}>{title}</h1>
-    </div>
-  );
+const Title: React.FC<{ title: string }> = ({ title }) => (
+  <div>
+    <h1 className={name(title)}>{title}</h1>
+  </div>
+);
 
 export default Title;
